e838d34bfebbb472019eca13331399c41d0164fb
Alexa Carrington <acarring@wellesley.edu>
2020-11-18 11:58:44 -0500

    Renamed png, added selector and functions to store.js

---
+++ store.js
@@ -0,0 +1,14 @@
+const removeCartItemButtons = document.getElementsByClassName('btn-danger');
+
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i];
+    button.addEventListener('click', function(event) {
+        let buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.remove();
+        updateCartTotal();
+    });
+}
+
+function updateCartTotal() {
+    
+}
